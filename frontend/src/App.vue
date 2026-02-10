194d807df031d5f7a726dd516fe250cab9c485fb
Smaran <smaranmaharjan22@gmail.com>
2026-02-10 22:32:08 +0900

    add

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import api from "./services/api";
+import { onMounted } from "vue";
+
+onMounted(async () => {
+  const res = await api.get("/");
+  console.log(res.data);
+});
+</script>
+
+<template>
+  <h1>Vue Frontend Running...</h1>
+</template>
